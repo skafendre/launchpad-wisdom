07fe12f524e14c9b88e818aa8226c2972af60e50
Eliott LAMBERT <eliott.lambert92@gmail.com>
2021-02-23 23:36:02 +0100

    added chord component

---
+++ components/Chord.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="chord-component">hello chord</div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        icon: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
