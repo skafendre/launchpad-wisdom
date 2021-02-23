12a379af2a0c685d7ab941cf3869ebd7d93b235a
Eliott LAMBERT <eliott.lambert92@gmail.com>
2021-02-23 23:05:19 +0100

    modified chord type

--- types.ts
+++ types.ts
@@ -1,13 +1,13 @@
 // todo convert hand and inversion to enum
 interface inversion {
-    hand: string
     inversion: string
     fingering: number[]
 }
 
 interface position {
-    name: number
-    inversions: inversion[]
+    position: number[]
+    leftHandInversions: inversion[]
+    rightHandInversions: inversion[]
 }
 
 export interface MappedChord {
